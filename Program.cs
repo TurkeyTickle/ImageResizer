6803a6db6bdb01ed8b7abe089e9b3f00529550bc
Nathan Lomax <nathanlomax@live.com>
2023-07-08 22:15:38 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+int maxWidth = int.Parse(args[1]);
+int maxHeight = int.Parse(args[2]);
+
+foreach (var file in Directory.GetFiles(args[0]))
+{
+    try
+    {
+        using var img = Image.Load(file);
+
+        double ratioX = maxWidth / (double)img.Width;
+        double ratioY = maxHeight / (double)img.Height;
+        double ratio = Math.Min(ratioX, ratioY);
+
+        var newWidth = (int)(img.Width * ratio);
+        var newHeight = (int)(img.Height * ratio);
+
+        img.Mutate(x => x.Resize(newWidth, newHeight));
+
+        await img.SaveAsJpegAsync(file);
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
